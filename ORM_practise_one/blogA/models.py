be5ca68f4850a871bb0a0b86ea7a14e6f3ab4746
Akonya John <johnakon>
2021-04-18 12:07:28 +0300

    working with the ORM

---
+++ ORM_practise_one/blogA/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateTimeField(blank=True, null=True)
+    author = models.ManyToManyField(Author, related_name="posts" )
+
+    def __str__(self):
+        return f'{self.title} | {self.content}'
+
+
+# making the author a choice option
